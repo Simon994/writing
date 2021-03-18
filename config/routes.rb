ae65cf53da4b78909bfd68f0107e37ec10a7398b
SimonN <65023744+Simon994@users.noreply.github.com>
2021-03-18 10:55:28 +0000

    WIP begin styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root 'categories#index'
   get '/categories' => 'categories#index'
   get '/categories/:id' => 'categories#show', as: :category
   get '/articles' => 'articles#index'
