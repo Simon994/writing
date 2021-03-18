77b13486ce4fd17b25793881caa84bfb8d0116c4
SimonN <65023744+Simon994@users.noreply.github.com>
2021-03-18 09:23:21 +0000

    Add seed data (WIP). Generate Categories controller. Add iroute to Categories controller index action and basic index view (WIP)

--- db/migrate/20210317160655_create_categories.rb
+++ db/migrate/20210317160655_create_categories.rb
@@ -4,8 +4,6 @@ class CreateCategories < ActiveRecord::Migration[6.1]
   def change
     create_table :categories do |t|
       t.string :name
-      t.string :image
-      t.string :description
       t.timestamps
     end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,3 +7,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+a1 = Article.create(title: 'Smart stretch expands hip implant performance', link: 'https://www.chemistryworld.com/news/smart-stretch-expands-hip-implant-performance/3008154.article',
+                    summary: 'On a material that offers to lengthen the lifetime of hip implants...')
+a2 = Article.create(title: 'Oil collector drums up hope for oceans', link: 'https://www.chemistryworld.com/news/oil-collector-drums-up-hope-for-oceans/3007800.article',
+                    summary: 'A two-faced barrel attracts then traps oil spills at speed...')
+
+c1 = Category.create(name: 'Journalism')
+c2 = Category.create(name: 'Materials science')
+c3 = Category.create(name: 'Environment')
+
+c1.articles << [a1, a2]
+c2.articles << [a1]
+c3.articles << [a2]
