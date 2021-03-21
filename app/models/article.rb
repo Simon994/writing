8a1ad4f50a9fad62839f717e2aa13425b56c397f
SimonN <65023744+Simon994@users.noreply.github.com>
2021-03-21 11:10:57 +0000

    Add basic validators

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,16 @@
 # frozen_string_literal: true
 
 class Article < ApplicationRecord
+  validates :title, presence: true, uniqueness: true
+  validates :link, presence: true, uniqueness: true
+  validates :summary, presence: true, uniqueness: true
+
+  validate :validate_length_of_summary
+
   has_many :parts
   has_many :categories, through: :parts
+
+  def validate_length_of_summary
+    errors.add(:summary, 'Length should be at least 40 and at most 200 characters') if summary.present? && ((summary.size < 40) || (summary.size > 200))
+  end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Category < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
   has_many :parts
   has_many :articles, through: :parts
 end
